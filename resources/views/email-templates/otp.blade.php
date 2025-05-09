1c9afc6891a341fa87479848be925e01703565fd
Deblina Das <deblina@keylines.net>
2025-05-09 15:52:38 +0530

    rest api

--- resources/views/email-templates/otp.blade.php
+++ resources/views/email-templates/otp.blade.php
@@ -18,13 +18,13 @@ $generalSetting             = GeneralSetting::find('1');
             <h3 style="text-align: center; font-size: 25px; color: #5c5b5b; font-family: sans-serif;">Hi, Welcome to <?=$generalSetting->site_name?>!</h3>
             <h4 style="text-align: center; font-family: sans-serif; color: #5c5b5b ;">Your OTP For Email Validation</h4>
             <div style="width: 100%; margin: 0 auto; display: block; text-align: center;">
-                <div style="padding: 12px; margin: 5px; border: 2px solid #f9233f;width: 17px; height: 17px; border-radius: 5px; display: block; text-align: center; font-size: 15px;
+                <div style="padding: 12px; margin: 5px; border: 2px solid #f9233f;width: 17px; height: 17px; border-radius: 5px; display: block; text-align: center; font-size: 15px; float: left;
                 font-family: sans-serif;"><?=substr($otp, 0, 1)?></div>
-                <div style="padding: 12px; margin: 5px; border: 2px solid #f9233f;width: 17px; height: 17px; border-radius: 5px; display: flex; justify-content: center; align-items: center; font-size: 15px;
+                <div style="padding: 12px; margin: 5px; border: 2px solid #f9233f;width: 17px; height: 17px; border-radius: 5px; display: flex; justify-content: center; align-items: center; font-size: 15px; float: left;
                 font-family: sans-serif;"><?=substr($otp, 1, 1)?></div>
-                <div style="padding: 12px; margin: 5px; border: 2px solid #f9233f;width: 17px; height: 17px; border-radius: 5px; display: flex; justify-content: center; align-items: center; font-size: 15px;
+                <div style="padding: 12px; margin: 5px; border: 2px solid #f9233f;width: 17px; height: 17px; border-radius: 5px; display: flex; justify-content: center; align-items: center; font-size: 15px; float: left;
                 font-family: sans-serif;"><?=substr($otp, 2, 1)?></div>
-                <div style="padding: 12px; margin: 5px; border: 2px solid #f9233f;width: 17px; height: 17px; border-radius: 5px; display: flex; justify-content: center; align-items: center; font-size: 15px;
+                <div style="padding: 12px; margin: 5px; border: 2px solid #f9233f;width: 17px; height: 17px; border-radius: 5px; display: flex; justify-content: center; align-items: center; font-size: 15px; float: left;
                 font-family: sans-serif;"><?=substr($otp, 3, 1)?></div>
                 <div style="padding: 12px; margin: 5px; border: 2px solid #f9233f;width: 17px; height: 17px; border-radius: 5px; display: flex; justify-content: center; align-items: center; font-size: 15px;
                 font-family: sans-serif;"><?=substr($otp, 4, 1)?></div>
