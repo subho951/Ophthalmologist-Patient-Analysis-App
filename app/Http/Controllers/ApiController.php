aa8fc07dd589aa3ffeb46e97787e5af15d23ce15
Deblina Das <deblina@keylines.net>
2025-05-09 12:47:41 +0530

    rest api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2297,7 +2297,7 @@ class ApiController extends Controller
                         'patient_mobile'                => $patientdetails->phone,
                         'affected_eye'                  => $patientdetails->eye,
                         'co-morbidities_id'             => $comorbidities->name,
-                        'doctor_name'                   => (($getDoctor)?$getDoctor->name:''),
+                        'doctor_name'                   => (($patientdetails)?$patientdetails->doctor_name:''),
                         'diagnosis_date'                => date_format(date_create($diagnosis_date), "Y-m-d"),                        
                         'test_result'                   => $test_result,                                                
                         'test_report_pdf'               => $test_report_pdf,
