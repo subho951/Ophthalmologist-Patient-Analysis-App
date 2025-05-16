92963482f52ac7ffe809434360214c4af3636060
Deblina Das <deblina@keylines.net>
2025-05-16 12:13:58 +0530

    api modification

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2378,7 +2378,7 @@ class ApiController extends Controller
                         'doctor_name'                   => (($patientdetails)?$patientdetails->doctor_name:''),
                         'diagnosis_date'                => date_format(date_create($diagnosis_date), "Y-m-d"),  
                         'test_score'                    => $test_score,
-                        'test_score_percentage'         => number_format(round($test_score_percentage,2),2),
+                        'test_score_percentage'         => $test_score_percentage,
                         'test_result'                   => $test_result,                                                
                         'test_report_pdf'               => $test_report_pdf,
                     ];
