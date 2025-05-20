8c9df2d8d7e5bb2aa59de6adf2fe9efbe9a7816f
surajit bera <surajit.b@keylines.net>
2025-05-20 13:12:58 +0530

    pdf work

--- resources/views/front/test-report-pdf.blade.php
+++ resources/views/front/test-report-pdf.blade.php
@@ -102,7 +102,7 @@
                 <table valign="top" style="width: 100%; border-collapse: collapse;">
                     <tr>
                         <td valign="top">
-                            <span class="label" style="width: 160px;">Patient’s Name <span style="float: right; margin-right: 2px; margin-top: -2px;">:</span></span>
+                            <span class="label" style="width: 160px;">Patient’s Name <span style="float: right; margin-right: 2px;">:</span></span>
                         </td>
                         <td valign="top">
                             <span class="value highlight"><?=(($getPatient)?$getPatient->name:'')?></span>
@@ -110,20 +110,20 @@
                     </tr>
                     <tr>
                         <td valign="top">
-                            <span class="label" style="width: 160px;">Patient’s Age <span style="float: right; margin-right: 2px; margin-top: -2px;">:</span></span>
+                            <span class="label" style="width: 160px;">Patient’s Age <span style="float: right; margin-right: 2px;">:</span></span>
                         </td>
                         <td valign="top"><span class="value"><?=(($getPatient)?$getPatient->age:'')?></span></td>
                     </tr>
                     <tr>
-                        <td valign="top"><span class="label" style="width: 160px;">Patient’s Gender <span style="float: right; margin-right: 2px; margin-top: -2px;">:</span></span></td>
+                        <td valign="top"><span class="label" style="width: 160px;">Patient’s Gender <span style="float: right; margin-right: 2px;">:</span></span></td>
                         <td valign="top"><span class="value"><?=(($getPatient)?(($getPatient->gender == 'F')?'Female':'Male'):'')?></span></td>
                     </tr>
                     <tr>
-                        <td valign="top"><span class="label" style="width: 160px;">Patient’s Contact No <span style="float: right; margin-right: 2px; margin-top: -2px;">:</span></span></td>
+                        <td valign="top"><span class="label" style="width: 160px;">Patient’s Contact No <span style="float: right; margin-right: 2px;">:</span></span></td>
                         <td valign="top"><span class="value"><?=(($getPatient)?$getPatient->phone:'')?></span></td>
                     </tr>
                     <tr>
-                        <td valign="top"><span class="label" style="width: 160px;">Affected Eye <span style="float: right; margin-right: 2px; margin-top: -2px;">:</span></span></td>
+                        <td valign="top"><span class="label" style="width: 160px;">Affected Eye <span style="float: right; margin-right: 2px;">:</span></span></td>
                         <td valign="top"><span class="value"><?=(($getPatient)?$getPatient->eye:'')?></span></td>
                     </tr>
                 </table>
@@ -131,7 +131,9 @@
             <div class="info-box">
                 <table>
                     <tr>
-                        <td valign="top"><span class="label" style="width: 160px;">Co-Morbidities <span style="float: right; margin-right: 2px; margin-top: -2px;">:</span></span></td>
+                        <td valign="top">
+                            <span class="label" style="width: 160px;">Co-Morbidities <span style="float: right; margin-right: 2px; margin-top: -2px;">:</span></span>
+                        </td>
                         <td valign="top">
                             <span class="value">
                                 <?php
@@ -156,9 +158,11 @@
                 <div class="info-box">
                     <table>
                         <tr>
-                            <td valign="top"><span class="label" style="width: 160px;">Co-Morbidities note <span style="float: right; margin-right: 2px; margin-top: -2px;">:</span></span></td>
                             <td valign="top">
-                                <span class="value">
+                                <span class="label" style="width: 160px;">Co-Morbidities note <span style="float: right; margin-right: 2px; margin-top: -2px;">:</span></span>
+                            </td>
+                            <td valign="top">
+                                <span class="value" style="display: inline">
                                     <?=(($getPatient)?$getPatient->comorbidities_note:'')?>
                                 </span>
                             </td>
@@ -169,7 +173,9 @@
             <div class="info-box">
                 <table>
                     <tr>
-                        <td valign="top"><span class="label" style="width: 160px;">Doctor’s Name <span style="float: right; margin-right: 2px; margin-top: -2px;">:</span></span></td>
+                        <td valign="top">
+                            <span class="label" style="width: 160px;">Doctor’s Name <span style="float: right; margin-right: 2px; margin-top: -2px;">:</span></span>
+                        </td>
                         <td valign="top"><span class="value"><?=$test_report->doctor_name?></span></td>
                     </tr>
                     <tr>
@@ -181,8 +187,10 @@
             <div class="info-box">
                 <table>
                     <tr>
-                        <td valign="middle"><span class="label" style="width: 160px;">Polypoidal Choroidal Vasculopathy Status</span></td>
-                        <td valign="middle"><span class="value"><span style="float: left; margin-right: 2px;">:</span><?=$test_report->test_result?></span></td>
+                        <td valign="middle">
+                            <span class="label" style="width: 160px;">Polypoidal Choroidal Vasculopathy Status</span>
+                        </td>
+                        <td valign="middle"><span class="value"><span style="float: left; display: inline;">:</span><?=$test_report->test_result?></span></td>
                     </tr>
                 </table>
             </div>
