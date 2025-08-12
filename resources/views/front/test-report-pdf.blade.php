ec21349ec5fb8217731298b57d04103962731dfb
shuvadeep Chakraborty <shuvadeep@keylines.net>
2025-08-12 15:59:45 +0530

    add test data pdf generate bug fixing

--- resources/views/front/test-report-pdf.blade.php
+++ resources/views/front/test-report-pdf.blade.php
@@ -81,7 +81,7 @@
             }
         </style>
     </head>
-    <body class="no-page-break" style="background:red;">
+    <body style="background:red;">
         <?php if($test_report){?>
         <?php
             $getPatient            = Patient::where('id', '=', $test_report->patient_id)->first();
