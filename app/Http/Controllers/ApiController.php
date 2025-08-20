85984bf4cb40c02986f1ab05cb6d0679ae2c7145
subhomoy samanta <subhomoy@keylines.net>
2025-08-20 15:11:35 +0530

    add test api updated

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2386,7 +2386,7 @@ class ApiController extends Controller
                         $dompdf->render();
                         $output                         = $dompdf->output();
                         // $dompdf->stream("document.pdf", array("Attachment" => true));die;
-                        $filename                       = $test_no.'.pdf';
+                        $filename                       = Helper::encoded($test_no).'.pdf';
                         $pdfFilePath                    = 'public/uploads/test-report/' . $filename;
                         file_put_contents($pdfFilePath, $output);
                         $test_report_pdf                = env('UPLOADS_URL').'test-report/' . $filename;
